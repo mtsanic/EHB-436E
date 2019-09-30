99030f01160997df26ccb81471c731202f54f82f
mtsanic <48995593+mtsanic@users.noreply.github.com>
2019-09-30 11:22:06 +0800

    Experiment-1
    • Become familiar with Xilinx ISE Software
    • Create a library which contains SSI Components

---
+++ Experiment-1/Top_Module.v
@@ -0,0 +1,4 @@
+`timescale 1ns / 1ps
+module Top_Module (input A,B, output C);
+AND GATE(A,B,C);
+endmodule
---
+++ Experiment-1/ring_oscillator.v
@@ -0,0 +1,14 @@
+`timescale 1ns / 1ps
+module ring_oscillator (input A, output C);
+(* KEEP = "TRUE" *)
+wire [199:0] W;
+genvar i;
+	generate
+	for (i =0; i < 199; i = i + 1)
+		begin:dfsf
+		NOT NOT0(W[i],W[i+1]);
+		end
+	endgenerate
+TRI TRI0 (W[199],A,W[0]);
+assign C = W[10];
+endmodule
